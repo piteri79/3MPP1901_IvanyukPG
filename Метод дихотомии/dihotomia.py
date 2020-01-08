b8cbdf86dfca3a880cc1b9382d076b69b43593f7
piteri79 <58482832+piteri79@users.noreply.github.com>
2020-01-08 17:14:09 +0300

    Add files via upload

--- Метод дихотомии/dihotomia.py
+++ Метод дихотомии/dihotomia.py
@@ -56,6 +56,9 @@ ed_eps.pack(side = 'left')
 #-- Счётчик количества итераций
 n = 0
 
+#-- Остановка обработки события мыши
+work_stop = True
+
 def calc(p,xx):
     return ym+(ym-(pow(2*p*(xx-10),2)+5))
 
@@ -71,30 +74,35 @@ canv.create_text(dw*13.2, dh, text='2', font=('Times New Roman', 12))
 
 #-- Слушатель событий мыши
 def listener(event):
-    global n
-    ed_a.configure(state="disabled"); ed_b.configure(state="disabled"); ed_eps.configure(state="disabled")
-    a = ed_a.get(); b = ed_b.get(); eps = ed_eps.get()
-    if a != '' and b != '' and eps != '':
-        a = int(a); b = int(b); eps = int(eps);
-        if a>0 and a<dt and b>0 and b<dt and eps>0 and eps<dt:
-            #-- Отобразить границы начального интервала
-            canv.create_line(dw+(dw*a), zh, dw+(dw*a), dh, fill='black', dash=4)
-            canv.create_line(dw+(dw*b), zh, dw+(dw*b), dh, fill='black', dash=4)
-            #-- Отобразить середину начального интервала
-            canv.create_line(dw+(dw*((a+b)/2)), zh, dw+(dw*((a+b)/2)), zh-dh*8, fill='black', dash=4)
-            #-- Отобразить границы точности начального интервала
-            canv.create_line(dw+(dw*((a+b-(eps/2))/2)), zh, dw+(dw*((a+b-(eps/2))/2)), zh-dh*8, fill='black', dash=4)
-            canv.create_line(dw+(dw*((a+b+(eps/2))/2)), zh, dw+(dw*((a+b+(eps/2))/2)), zh-dh*8, fill='black', dash=4)
-            while (b-a) > eps:
-                x1 = (a+b-(eps/2))/2
-                x2 = (a+b+(eps/2))/2
-                n = n + 1
-                if calc(p,x2) < calc(p,x1): b = x2
-                elif calc(p,x2) > calc(p,x1): a = x1
-            x_zv = (a+b)/2
-            fx = calc(p,x_zv)
-            canv.create_line(dw+(dw*x_zv), zh, dw+(dw*x_zv), fx, fill='black', dash=4)
-            canv.create_text(dw*9, dh*5, text='x* = min{x : f('+str(a)+'), f('+str(b)+')}, f('+str(fx-ym*2)+'), n = '+str(n), font=('Times New Roman', 12))
+    global n,work_stop
+    if work_stop == True:
+        ed_a.configure(state="disabled"); ed_b.configure(state="disabled"); ed_eps.configure(state="disabled")
+        a = ed_a.get(); b = ed_b.get(); eps = ed_eps.get()
+        if a != '' and b != '' and eps != '':
+            a = int(a); b = int(b); eps = int(eps);
+            if a>0 and a<dt and b>0 and b<dt and eps>0 and eps<dt:
+                #-- Отобразить границы начального интервала
+                canv.create_line(dw+(dw*a), zh, dw+(dw*a), dh, fill='black', dash=4)
+                canv.create_line(dw+(dw*b), zh, dw+(dw*b), dh, fill='black', dash=4)
+                #-- Отобразить середину начального интервала
+                canv.create_line(dw+(dw*((a+b)/2)), zh, dw+(dw*((a+b)/2)), zh-dh*8, fill='black', dash=4)
+                #-- Отобразить границы точности начального интервала
+                canv.create_line(dw+(dw*((a+b-(eps/2))/2)), zh, dw+(dw*((a+b-(eps/2))/2)), zh-dh*8, fill='black', dash=4)
+                canv.create_line(dw+(dw*((a+b+(eps/2))/2)), zh, dw+(dw*((a+b+(eps/2))/2)), zh-dh*8, fill='black', dash=4)
+                while (b-a) > eps:
+                    x1 = (a+b-(eps/2))/2
+                    x2 = (a+b+(eps/2))/2
+                    n = n + 1
+                    if calc(p,x2) < calc(p,x1): b = x2
+                    elif calc(p,x2) > calc(p,x1): a = x1
+                x_zv = (a+b)/2
+                fx = calc(p,x_zv)
+                canv.create_line(dw+(dw*x_zv), zh, dw+(dw*x_zv), fx, fill='black', dash=4)
+                canv.create_line(dw, fx, dw+(dw*x_zv), fx, fill='black', dash=4)
+                fx_txt = pow(2*p*(x_zv-10),2)+5
+                canv.create_text(dw*9, dh*5, text='x* = min{x : f('+str(a)+'), f('+str(b)+')}', font=('Times New Roman', 14))
+                canv.create_text(dw*9, dh*6, text='x* = '+str(x_zv)+', f('+str(fx_txt)+'), n = '+str(n), font=('Times New Roman', 14))
+                work_stop = False
 
 canv.bind('<Button-1>', listener)
 
