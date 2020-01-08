f134e53e22704c3ab23ece13e789b9c623c2a402
piteri79 <58482832+piteri79@users.noreply.github.com>
2020-01-08 19:59:32 +0300

    Add files via upload

--- Метод дихотомии/dihotomia.py
+++ Метод дихотомии/dihotomia.py
@@ -30,9 +30,9 @@ t=0
 dt=16
 while t<dt:
     canv.create_line(zw+(dw*t), zh+5, zw+(dw*t), zh-5, fill='black', dash=4)
-    canv.create_text(dw+(dw*t)+10, zh+10, text=str(t), font=('Times New Roman', 10))
+    canv.create_text(zw+(dw*t)+10, zh+10, text=str(t), font=('Times New Roman', 10))
     canv.create_line(dw-5, zh-(dh*t), dw+5, zh-(dh*t), fill='black', dash=4)
-    canv.create_text(dw-10, zh-(dw*t)-10, text=str(t), font=('Times New Roman', 10))
+    canv.create_text(zw-10, zh-(dw*t)-10, text=str(t*10), font=('Times New Roman', 10))
     t += 1
 
 lb_txt = Label(pn_control, text=u"Введите начальную и конечную точки и точность вычисления и кликните левой кнопкой мыши")
@@ -60,14 +60,14 @@ n = 0
 work_stop = True
 
 def calc(p,xx):
-    return ym+(ym-(pow(2*p*(xx-10),2)+5))
+    return zh-(pow(2*p*(xx-10),2)+5)
 
 #-- Рисуем график функции
 xg=2; p=1
-yg=pow(2*p*(xg-10),2)+5; ym=yg
+yg=zh-(pow(2*p*(xg-10),2)+5);
 for xx in range(2,16, 1):
-    yy = ym+(ym-(pow(2*p*(xx-10),2)+5))
-    canv.create_line(xg*dw+dw, yg, xx*dw+dw, yy, fill='blue')
+    yy = zh-(pow(2*p*(xx-10),2)+5)
+    canv.create_line(xg*dw+zw, yg, xx*dw+zw, yy, fill='blue')
     xg = xx; yg = yy
 canv.create_text(dw*14, dh*1.3, text='y  = 2px', font=('Times New Roman', 24))
 canv.create_text(dw*13.2, dh, text='2', font=('Times New Roman', 12))
@@ -82,13 +82,13 @@ def listener(event):
             a = int(a); b = int(b); eps = int(eps);
             if a>0 and a<dt and b>0 and b<dt and eps>0 and eps<dt:
                 #-- Отобразить границы начального интервала
-                canv.create_line(dw+(dw*a), zh, dw+(dw*a), dh, fill='black', dash=4)
-                canv.create_line(dw+(dw*b), zh, dw+(dw*b), dh, fill='black', dash=4)
+                canv.create_line(zw+(dw*a), zh, zw+(dw*a), dh, fill='black', dash=4)
+                canv.create_line(zw+(dw*b), zh, zw+(dw*b), dh, fill='black', dash=4)
                 #-- Отобразить середину начального интервала
-                canv.create_line(dw+(dw*((a+b)/2)), zh, dw+(dw*((a+b)/2)), zh-dh*8, fill='black', dash=4)
+                canv.create_line(zw+(dw*((a+b)/2)), zh, zw+(dw*((a+b)/2)), zh-dh*8, fill='black', dash=4)
                 #-- Отобразить границы точности начального интервала
-                canv.create_line(dw+(dw*((a+b-(eps/2))/2)), zh, dw+(dw*((a+b-(eps/2))/2)), zh-dh*8, fill='black', dash=4)
-                canv.create_line(dw+(dw*((a+b+(eps/2))/2)), zh, dw+(dw*((a+b+(eps/2))/2)), zh-dh*8, fill='black', dash=4)
+                canv.create_line(zw+(dw*((a+b-(eps/2))/2)), zh, zw+(dw*((a+b-(eps/2))/2)), zh-dh*8, fill='black', dash=4)
+                canv.create_line(zw+(dw*((a+b+(eps/2))/2)), zh, zw+(dw*((a+b+(eps/2))/2)), zh-dh*8, fill='black', dash=4)
                 while (b-a) > eps:
                     x1 = (a+b-(eps/2))/2
                     x2 = (a+b+(eps/2))/2
@@ -97,7 +97,7 @@ def listener(event):
                     elif calc(p,x2) > calc(p,x1): a = x1
                 x_zv = (a+b)/2
                 fx = calc(p,x_zv)
-                canv.create_line(dw+(dw*x_zv), zh, dw+(dw*x_zv), fx, fill='black', dash=4)
+                canv.create_line(zw, fx, zw+(dw*x_zv), fx, fill='black', dash=4)
                 fx_txt = pow(2*p*(x_zv-10),2)+5
                 canv.create_text(dw*9, dh*5, text='x* = min{x : f('+str(a)+'), f('+str(b)+')}', font=('Times New Roman', 14))
                 canv.create_text(dw*9, dh*6, text='x* = '+str(x_zv)+', f(x*) = '+str(fx_txt)+', n = '+str(n), font=('Times New Roman', 14))
